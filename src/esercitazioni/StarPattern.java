da7e1485e70eed15f5f486496cb6f48212b6329f
Dario <104058513+DarioIEL@users.noreply.github.com>
2025-03-24 11:39:49 +0100

    Add files via upload

---
+++ src/esercitazioni/StarPattern.java
@@ -0,0 +1,70 @@
+package esercitazioni;
+
+public class StarPattern {
+
+	public static void main(String[] args) {
+		//Riprodurre il seguente pattern
+		//  *****
+		//  *****
+		//  *****
+		//  *****
+		//  *****
+		
+		//STAMPA IL SEGUENTE PATTERN
+		// *
+		// * *
+		// * * *
+		// * * * *
+		// * * * * *
+		
+		
+		//STAMPA IL SEGUENTE PATTERN
+		//* * * * * 
+		//* * * *
+		//* * *
+		//* *
+		//*
+		
+		
+		
+		//STAMPA IL SEGUENTE PATTERN
+		//  	*
+		//     * *
+		//    * * *
+		//   * * * *
+		//  * * * * *
+		
+		
+		//STAMPA IL SEGUENTE PATTERN
+		// * * * * *
+		//  * * * *
+		//   * * *
+		//    * * 
+		//     *
+		
+		
+		//STAMPA IL SEGUENTE PATTERN
+		//	    *
+		//     * *
+		//    * * *
+		//   * * * *
+		//  * * * * *
+		//   * * * *
+		//    * * *
+		//     * * 
+		//      *
+		
+		
+		//STAMPA IL SEGUENTE PATTERN
+		//	    *
+		//     * *
+		//    *   *
+		//   *     *
+		//  *       *
+		//   *     *
+		//    *   *
+		//     * * 
+		//      *
+		
+	}
+}
